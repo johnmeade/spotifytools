b0c766c514f628e6e690af79293286f27e0e0c91
John Meade <john.meade@hbar.ca>
2020-07-27 03:10:40 +0000

    album shuffle api

--- app.sh
+++ app.sh
@@ -3,4 +3,4 @@ set -e
 source ./secret/auth.sh
 source .venv/bin/activate
 export FLASK_APP=spotifytools
-python -m flask run --port=9224
+python -m flask run --port 9224 --host 0.0.0.0
--- spotifytools/__init__.py
+++ spotifytools/__init__.py
@@ -1,8 +1,11 @@
-import os
-from pathlib import Path
+from .actions import perform, shuffle_albums
+
 from flask import Flask, session, request, redirect
 from flask_session import Session
 import spotipy
+
+from pathlib import Path
+import os
 
 
 app = Flask(__name__)
@@ -11,8 +14,12 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 Session(app)
 
-CACHE = Path(__file__).parent.parent.joinpath('cache')
-auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=CACHE)
+CACHE = Path('~', '.cache', 'spotifytools').expanduser()
+SCOPES = ','.join([
+    'user-modify-playback-state',
+    'user-library-read',
+])
+auth_manager = spotipy.oauth2.SpotifyOAuth(scope=SCOPES, cache_path=CACHE)
 spotify = spotipy.Spotify(auth_manager=auth_manager)
 
 
@@ -24,11 +31,13 @@ def index():
 
     if not session.get('token_info'):
         auth_url = auth_manager.get_authorize_url()
-        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+        return f'<h2><a href="{auth_url}">Authorize with Spotify</a></h2>'
 
-    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">my playlists</a>'
+    return (
+        f'<h2>Logged in as: {spotify.me()["display_name"]}  '
+        f'<a href="/sign_out">[sign out]<a/></h2>'
+        f'<a href="/album_shuffle">Shuffle my liked albums</a>'
+    )
 
 
 @app.route('/sign_out')
@@ -37,9 +46,14 @@ def sign_out():
     return redirect('/')
 
 
-@app.route('/playlists')
+@app.route('/album_shuffle')
 def playlists():
     if not session.get('token_info'):
         return redirect('/')
     else:
-        return spotify.current_user_playlists()
+        perform(shuffle_albums, (spotify,))
+        return (
+            '<p>You queue will shortly populate with up to 25 random liked albums.</p>'
+            '<p>You can leave this page.</p>'
+            '<a href="/">Home</a>'
+        )
