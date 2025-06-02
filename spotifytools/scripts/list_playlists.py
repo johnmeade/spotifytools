57003ce010e9e737b98b9013c159068aa3790e63
John Meade <john.meade@hbar.ca>
2025-06-02 17:01:05 +0000

    fix caching

---
+++ spotifytools/scripts/list_playlists.py
@@ -0,0 +1,25 @@
+import spotipy
+
+from pathlib import Path
+import pickle
+
+
+HERE = Path(__file__).parent
+
+
+def main():
+    # spotipy init
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-library-read', cache_path='/tmp/spotipy')
+    api = spotipy.Spotify(auth_manager=auth_manager)
+    # query
+    print('Fetching playlists')
+    pls = api.current_user_playlists()
+    print('---')
+    print(pls)
+    print('---')
+    for p in pls:
+        print(p['name'])
+
+
+if __name__ == '__main__':
+    main()
